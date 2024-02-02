6a41849d29a5a55c4de0665cd2f153205576c4df
Brayan Erazo <erazo2498@gmail.com>
2024-02-02 12:39:12 -0500

    Se agrega controlador de excepciones y se añade el manejador de excepciones para las validaciones (anotaciones) que permite jakarta

---
+++ src/main/java/com/quind/pruebatecnica/configuration/ControllerAdvisor.java
@@ -0,0 +1,28 @@
+package com.quind.pruebatecnica.configuration;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestControllerAdvice
+public class ControllerAdvisor {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleValidationException(MethodArgumentNotValidException ex) {
+        List<String> errorMessages = new ArrayList<>();
+        for (ObjectError error : ex.getBindingResult().getAllErrors()) {
+            if (error instanceof FieldError fieldError) {
+                errorMessages.add(fieldError.getField() + ": " + fieldError.getDefaultMessage());
+            } else {
+                errorMessages.add(error.getDefaultMessage());
+            }
+        }
+        return new ResponseEntity<>(errorMessages, HttpStatus.BAD_REQUEST);
+    }
+}
