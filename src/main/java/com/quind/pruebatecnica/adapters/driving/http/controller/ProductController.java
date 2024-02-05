845da2c00a90e6884f562ed91bf2867724e99389
Brayan Erazo <erazo2498@gmail.com>
2024-02-05 03:25:34 -0500

    Se agrega nueva configuracion de swagger para productos

--- src/main/java/com/quind/pruebatecnica/adapters/driving/http/controller/ProductController.java
+++ src/main/java/com/quind/pruebatecnica/adapters/driving/http/controller/ProductController.java
@@ -38,24 +38,59 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(Collections.singletonMap(Constants.RESPONSE_MESSAGE_KEY, Constants.PRODUCT_CREATED_MESSAGE));
     }
+    @Operation(summary = "get product by id",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "returned product",
+                            content = @Content(mediaType = "application/json", schema = @Schema(ref = "#/components/schemas/Map"))),
+                    @ApiResponse(responseCode = "404", description = "product not found",
+                            content = @Content(mediaType = "application/json", schema = @Schema(ref = "#/components/schemas/Error")))
+            })
     @GetMapping("/{productId}")
-    public ResponseEntity<ProductResponseDto> getProduct(@NotNull @PathVariable Long productId){
+    public ResponseEntity<ProductResponseDto> getProduct(@PathVariable Long productId){
         return ResponseEntity.status(HttpStatus.OK)
                 .body(productHandler.getProduct(productId));
     }
 
+    @Operation(summary = "activate product by id",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "returned product",
+                            content = @Content(mediaType = "application/json", schema = @Schema(ref = "#/components/schemas/Map"))),
+                    @ApiResponse(responseCode = "404", description = "product not found",
+                            content = @Content(mediaType = "application/json", schema = @Schema(ref = "#/components/schemas/Error"))),
+                    @ApiResponse(responseCode = "422", description = "product is already activated",
+                            content = @Content(mediaType = "application/json", schema = @Schema(ref = "#/components/schemas/Error")))
+            })
     @PutMapping("/activate/{id}")
     public ResponseEntity<Map<String,String>> activateProduct(@PathVariable Long id){
         productHandler.activateProduct(id);
         return ResponseEntity.status(HttpStatus.OK)
                 .body(Collections.singletonMap(Constants.RESPONSE_MESSAGE_KEY, Constants.PRODUCT_ACTIVATED_MESSAGE));
     }
+    @Operation(summary = "inactivate product by id",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "returned product",
+                            content = @Content(mediaType = "application/json", schema = @Schema(ref = "#/components/schemas/Map"))),
+                    @ApiResponse(responseCode = "404", description = "product not found",
+                            content = @Content(mediaType = "application/json", schema = @Schema(ref = "#/components/schemas/Error"))),
+                    @ApiResponse(responseCode = "422", description = "product is already inactivated",
+                            content = @Content(mediaType = "application/json", schema = @Schema(ref = "#/components/schemas/Error")))
+            })
     @PutMapping("/inactivate/{id}")
     public ResponseEntity<Map<String,String>> inactivateProduct(@PathVariable Long id){
         productHandler.inactivateProduct(id);
         return ResponseEntity.status(HttpStatus.OK)
                 .body(Collections.singletonMap(Constants.RESPONSE_MESSAGE_KEY, Constants.PRODUCT_INACTIVATED_MESSAGE));
     }
+
+    @Operation(summary = "cancel product by id",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "returned product",
+                            content = @Content(mediaType = "application/json", schema = @Schema(ref = "#/components/schemas/Map"))),
+                    @ApiResponse(responseCode = "404", description = "product not found",
+                            content = @Content(mediaType = "application/json", schema = @Schema(ref = "#/components/schemas/Error"))),
+                    @ApiResponse(responseCode = "422", description = "product is already canceled or balance different to zero",
+                            content = @Content(mediaType = "application/json", schema = @Schema(ref = "#/components/schemas/Error")))
+            })
     @PutMapping("/cancel/{id}")
     public ResponseEntity<Map<String,String>> cancelProduct(@PathVariable Long id){
         productHandler.cancelProduct(id);
